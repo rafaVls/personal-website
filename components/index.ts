c26b0eebcf7a76475cd9e11cbeea6201020aa238
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-07 12:32:24 -0700

    feat: :sparkles: Add Footer import/export to index.ts

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import About from "./About/About";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Portfolio from "./Portfolio/Portfolio";
+import Footer from "./Footer/Footer";
 
-export { About, Header, Main, Portfolio };
+export { About, Header, Main, Portfolio, Footer };
