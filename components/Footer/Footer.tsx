374d6063bb14c16bc55be57800f5a379a3975984
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-07 13:31:02 -0700

    feat: :sparkles: Add Footer content and more styling.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,23 @@ import styles from "./Footer.module.css";
 export default function Footer(): JSX.Element {
 	return (
 		<footer className={styles.footer}>
-			<p>This is a footer &copy;</p>
+			<ul>
+				<li>
+					<a
+						href="https://twitter.com/RafaelAvls"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Twitter
+					</a>
+				</li>
+				<li>
+					<a href="https://github.com/rafaVls" target="_blank" rel="noreferrer">
+						Github
+					</a>
+				</li>
+			</ul>
+			<p>&copy; Rafael Avilés 2021</p>
 		</footer>
 	);
 }
