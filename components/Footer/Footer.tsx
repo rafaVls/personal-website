3d7cfe74cc2ec846471f9fabe155e2e0f03d4ddf
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-07 12:31:34 -0700

    feat: :lipstick: Add Footer's stylesheet.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.css";
+
 export default function Footer(): JSX.Element {
 	return (
-		<footer>
+		<footer className={styles.footer}>
 			<p>This is a footer &copy;</p>
 		</footer>
 	);
