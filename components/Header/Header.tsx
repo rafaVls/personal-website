cd3d2d039f1c5e2e2a0ea248481dd2cdabaa5c3b
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-07 14:19:24 -0700

    fix: :bug: Add paths to homepage and blog page in Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,18 +5,18 @@ export default function Header(): JSX.Element {
 		<>
 			<header className={styles.container}>
 				<h1>
-					<a href="#">&lt;Rafael /&gt;</a>
+					<a href="/">&lt;Rafael /&gt;</a>
 				</h1>
 				<nav className={styles.navBar}>
 					<ul className={styles.navList}>
 						<li>
-							<a href="#">Home</a>
+							<a href="/">Home</a>
 						</li>
 						<li>
 							<a href="#about">About</a>
 						</li>
 						<li>
-							<a href="#">Blog</a>
+							<a href="/blog">Blog</a>
 						</li>
 						<li>
 							<a href="#portfolio">Portfolio</a>
