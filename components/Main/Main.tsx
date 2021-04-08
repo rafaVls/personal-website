528671ccccfd30d1298cdd37a029caa2dc717327
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-07 23:21:04 -0700

    feat: :dizzy: Add animation for Main section of homepage.

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,6 +1,37 @@
+import { MutableRefObject, useEffect, useRef } from "react";
 import styles from "./Main.module.css";
 
+function textSequence(
+	i: number,
+	textElement: MutableRefObject<HTMLParagraphElement>
+): void {
+	const elementCurrent = textElement.current;
+	const professions = [
+		"UI & UX Designer",
+		"Technical Writer",
+		"Renewables Engineer",
+		"Web Developer"
+	];
+
+	if (professions.length > i) {
+		setTimeout(() => {
+			elementCurrent.dataset.after = professions[i];
+			textSequence(++i, textElement);
+		}, 4250);
+	} else if (professions.length === i) {
+		textSequence(0, textElement);
+	}
+}
+
 export default function Main(): JSX.Element {
+	const paragraph: MutableRefObject<HTMLParagraphElement> = useRef();
+
+	useEffect(() => {
+		if (typeof paragraph.current !== "undefined") {
+			textSequence(0, paragraph);
+		}
+	}, []);
+
 	return (
 		<main className={styles.main}>
 			<section>
@@ -8,7 +39,15 @@ export default function Main(): JSX.Element {
 				<p className={styles.bigText}>
 					My name is <strong>Rafael Avilés</strong>
 				</p>
-				<p className={styles.normalText}>I am a Web Developer</p>
+				<div>
+					<p
+						className={`${styles.normalText} ${styles.typewriter}`}
+						ref={paragraph}
+						data-after="Web Developer"
+					>
+						I am a{" "}
+					</p>
+				</div>
 			</section>
 			<img src="images/laptop.svg" alt="A laptop creating a website." />
 			<a href="#about">&gt;</a>
