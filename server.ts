8aae86b7375962ffc49f96395e5c763b73e8d0b0
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-12 11:34:07 -0700

    feat(Server): :sparkles: Add GhostContentAPI to server.ts.
    This API lets me get all posts from my Ghost CMS. I'll use this to show them in my Nextjs frontend.

--- server.ts
+++ server.ts
@@ -1,10 +1,39 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import express from "express";
+const GhostContentAPI = require("@tryghost/content-api");
 const app = express();
-const port = 5000;
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+dotenv.config({ path: "./.env.local" });
 
-app.get("/", (_, res) => {
-	res.status(200).send("ok");
+const api = new GhostContentAPI({
+	url: process.env.GHOST_HOST,
+	key: process.env.GHOST_API_KEY,
+	version: "v3"
 });
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+async function getPosts(): Promise<void> {
+	interface Post {
+		title: string;
+	}
+
+	try {
+		const posts = await api.posts.browse({
+			filter: "tag:getting-started"
+		});
+		posts.forEach((post: Post) => {
+			console.log(post.title);
+		});
+	} catch (err) {
+		console.log(err.message);
+	}
+}
+
+app.get("/", (req, res) => {
+	res.status(200).send("Hello from server!");
+	getPosts();
+});
+
+app.listen(port, () =>
+	console.log(`App listening on http://localhost:${port}`)
+);
