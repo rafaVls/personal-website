34afaebc3f021bfce6b9989422a0fdd811c892df
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-16 11:48:35 -0700

    feat(Frontend): :sparkles: Add code to retreive data needed to display a post from API.

--- frontend/pages/posts/[slug].tsx
+++ frontend/pages/posts/[slug].tsx
@@ -1,10 +1,38 @@
 import { GetStaticProps, InferGetStaticPropsType, GetStaticPaths } from "next";
+import parse from "html-react-parser";
 import { server } from "../../config";
 
+// TODO: Use html-react-parser to convert fetched post's html to jsx safely
 export default function Post({
 	post
 }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
-	return <h1>{post.title}</h1>;
+	const postContent = parse(post.html);
+
+	return (
+		<article>
+			<header>
+				<a>{post.primary_tag.name}</a>
+				<h2>{post.title}</h2>
+				<p>{post.excerpt}</p>
+				<section>
+					<figure>
+						<img
+							src={post.primary_author.profile_image}
+							alt={post.primary_author.name}
+						/>
+					</figure>
+					<p>
+						{post.published_at} <strong> · </strong> {post.reading_time} minute
+						read
+					</p>
+				</section>
+			</header>
+			<section>
+				<img src={post.feature_image} alt={post.title} />
+				{postContent}
+			</section>
+		</article>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
