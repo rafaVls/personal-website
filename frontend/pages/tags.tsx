565312d7c785cbe7e3985ad816b39458d4126eba
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-22 13:10:44 -0700

    feat(Frontend): :sparkles: Add tag and post elements to return statement.

--- frontend/pages/tags.tsx
+++ frontend/pages/tags.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from "next";
+import Link from "next/link";
+import { Fragment } from "react";
 import { Tag } from "../common/types";
 import { getTags } from "../utils/helpers";
 
@@ -7,7 +9,22 @@ interface Props {
 }
 
 export default function Tags({ tags }: Props): JSX.Element {
-	return <div>You got here!</div>;
+	const tagsContent = tags.map((tag, i) => (
+		<Fragment key={i}>
+			<h2>{tag.name}</h2>
+			<ul>
+				{tag.posts.map((post, j) => (
+					<li key={j}>
+						<Link href={`/tags/${tag.slug}`}>
+							<a>{post.title}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</Fragment>
+	));
+
+	return <article>{tagsContent}</article>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
