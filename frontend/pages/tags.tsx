25b1edb987112082b7995c50a9ca9f0f696c570c
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-22 12:35:19 -0700

    feat(Frontend): :sparkles: Add getStaticProps to tags.tsx to get all tags from the server.

--- frontend/pages/tags.tsx
+++ frontend/pages/tags.tsx
@@ -1,3 +1,20 @@
-export default function Tags(): JSX.Element {
+import { GetStaticProps } from "next";
+import { Tag } from "../common/types";
+import { server } from "../config";
+
+interface Props {
+	tags: Tag[];
+}
+
+export default function Tags({ tags }: Props): JSX.Element {
 	return <div>You got here!</div>;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const res = await fetch(`${server}/tags`);
+	const tags: Tag[] = await res.json();
+
+	return {
+		props: { tags }
+	};
+};
