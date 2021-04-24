5e3c67f80a17d00388f57035a001a3eba6edb9ce
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-23 22:01:08 -0700

    feat(Frontend): :sparkles: Add button for copying code text.

--- frontend/components/Highlight/Highlight.tsx
+++ frontend/components/Highlight/Highlight.tsx
@@ -2,8 +2,9 @@
 //* It's made based on the node package react-highlight.js:
 //? https://github.com/bvaughn/react-highlight.js
 
-import { ReactNode, useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef, MouseEvent } from "react";
 import highlight from "highlight.js";
+import styles from "./Highlight.module.css";
 
 interface Props {
 	children: ReactNode;
@@ -19,9 +20,33 @@ export default function Highlight({ children, language }: Props): JSX.Element {
 		}
 	}, []);
 
+	function clickHandler(
+		e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+	): void {
+		e.preventDefault();
+
+		// This selects the contents of the code block and copies them to the clipboard.
+		if (window.getSelection) {
+			const selection = window.getSelection();
+			const range = document.createRange();
+
+			range.selectNodeContents(codeElement.current);
+			selection.removeAllRanges();
+			selection.addRange(range);
+			document.execCommand("copy");
+		} else {
+			console.warn("Could not select text: Unsopported browser.");
+		}
+	}
+
 	return (
-		<code className={language} ref={codeElement}>
-			{children}
-		</code>
+		<>
+			<button className={styles.copyButton} onClick={e => clickHandler(e)}>
+				Copy
+			</button>
+			<code className={language} ref={codeElement}>
+				{children}
+			</code>
+		</>
 	);
 }
