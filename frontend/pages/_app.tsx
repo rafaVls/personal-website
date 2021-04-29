5cbe09fe05fd9a6f96cce9ab2526cfbcb2d98239
Rafael Aviles <rafa.vfierro@outlook.com>
2021-04-29 14:52:16 -0700

    style(Frontend): :art: Change around import statements and add some whitespace.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import { Layout } from "../components/index";
+
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
