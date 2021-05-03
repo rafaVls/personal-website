4294ae779bc427b5e59c258e957d8a087d64dde4
Rafael Aviles <rafa.vfierro@outlook.com>
2021-05-03 15:49:51 -0700

    feat: :bento: Add icons.

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,24 @@ export default function Home(): DetailedHTMLProps<
 					name="description"
 					content="Landing page where you can see my skills, my portfolio containing my projects, and my education."
 				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
 				<title>Rafael Avilés | Web Developer</title>
 			</Head>
 			<Main />
