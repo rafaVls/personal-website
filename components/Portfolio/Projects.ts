05b464860248839f55997b9296eb930951a0c9fa
Rafael Avilés <rafa.vfierro@outlook.com>
2021-10-07 16:16:23 -0700

    chore: add Typefight to projects

--- components/Portfolio/Projects.ts
+++ components/Portfolio/Projects.ts
@@ -9,11 +9,28 @@ export interface Project {
 
 const Projects: Project[] = [
 	{
+		img: "/images/projects/typefight.png",
+		title: "Typefight",
+		description:
+			"Typefight is a typing game where you get a random sentence, type it in, and see your score, as well as other people's score.",
+		stack: [
+			"Python",
+			"Flask",
+			"Docker",
+			"PostgreSQL",
+			"HTML",
+			"CSS",
+			"JavaScript"
+		],
+		link: "",
+		repo: "https://github.com/rafaVls/typefight"
+	},
+	{
 		img: "/images/projects/weatherly.png",
 		title: "Weatherly",
 		description:
-			"A fullstack weather app made with Reactjs, Expressjs and Typescript.",
-		stack: ["Expressjs", "React", "Typescript"],
+			"A fullstack weather app made with Reactjs, Expressjs and TypeScript.",
+		stack: ["Expressjs", "React", "TypeScript"],
 		link: "",
 		repo: "https://github.com/rafaVls/Fullstack-Weatherly"
 	},
@@ -22,7 +39,7 @@ const Projects: Project[] = [
 		title: "Personal Website",
 		description:
 			"This is a website made using Next.js, a static site generator; and Ghost, a headless cms for the blog section.",
-		stack: ["Node", "Nextjs", "Ghost CMS", "Typescript"],
+		stack: ["Node", "Nextjs", "Ghost CMS", "TypeScript"],
 		link: "/",
 		repo: "https://github.com/rafaVls/personal-website"
 	},
@@ -31,7 +48,7 @@ const Projects: Project[] = [
 		title: "Calculator app",
 		description:
 			"This app is a solution to the Frontend Mentor calculator challenge. Some of the math functionailty is still a work in progress.",
-		stack: ["HTML", "CSS", "Javascript"],
+		stack: ["HTML", "CSS", "JavaScript"],
 		link: "https://frontend-calculator.herokuapp.com/",
 		repo: "https://github.com/rafaVls/calculator-app"
 	}
