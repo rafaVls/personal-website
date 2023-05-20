4518c015b922ee36b98143c35c91c87422ba798f
Rafael Avilés <rafa.vfierro@outlook.com>
2023-05-20 13:49:39 -0700

    fix: update calculator preview site and repo links

--- components/Portfolio/Projects.ts
+++ components/Portfolio/Projects.ts
@@ -1,57 +1,51 @@
 export interface Project {
-	img: string;
-	title: string;
-	description: string;
-	stack: string[];
-	link: string;
-	repo: string;
+  img: string;
+  title: string;
+  description: string;
+  stack: string[];
+  link: string;
+  repo: string;
 }
 
 const Projects: Project[] = [
-	{
-		img: "/images/projects/typefight.png",
-		title: "Typefight",
-		description:
-			"Typefight is a typing game where you get a random sentence, type it in, and compare your time with a global highscore",
-		stack: [
-			"Python",
-			"Flask",
-			"Docker",
-			"PostgreSQL",
-			"HTML",
-			"CSS",
-			"JavaScript"
-		],
-		link: "https://typefight.herokuapp.com/",
-		repo: "https://gitlab.com/rafaVls/typefight"
-	},
-	{
-		img: "/images/projects/weatherly.png",
-		title: "Weatherly",
-		description:
-			"A fullstack weather app made with Reactjs, Flask and TypeScript.",
-		stack: ["React", "TypeScript", "Python", "Flask", "CSS"],
-		link: "https://weatherly-sand.vercel.app",
-		repo: "https://github.com/rafaVls/Weatherly"
-	},
-	{
-		img: "/images/projects/homepage.png",
-		title: "Personal Website",
-		description:
-			"This is a website made using Next.js, a static site generator; and Ghost, a headless cms for the blog section.",
-		stack: ["Node", "Nextjs", "Ghost CMS", "TypeScript"],
-		link: "/",
-		repo: "https://github.com/rafaVls/personal-website"
-	},
-	{
-		img: "/images/projects/calculator.png",
-		title: "Calculator app",
-		description:
-			"This app is a solution to the Frontend Mentor calculator challenge. Some of the math functionailty is still a work in progress.",
-		stack: ["HTML", "CSS", "JavaScript"],
-		link: "https://frontend-calculator.herokuapp.com/",
-		repo: "https://github.com/rafaVls/calculator-app"
-	}
+  {
+    img : "/images/projects/typefight.png",
+    title : "Typefight",
+    description :
+        "Typefight is a typing game where you get a random sentence, type it in, and compare your time with a global highscore",
+    stack : [
+      "Python", "Flask", "Docker", "PostgreSQL", "HTML", "CSS", "JavaScript"
+    ],
+    link : "https://typefight.herokuapp.com/",
+    repo : "https://gitlab.com/rafaVls/typefight"
+  },
+  {
+    img : "/images/projects/weatherly.png",
+    title : "Weatherly",
+    description :
+        "A fullstack weather app made with Reactjs, Flask and TypeScript.",
+    stack : [ "React", "TypeScript", "Python", "Flask", "CSS" ],
+    link : "https://weatherly-sand.vercel.app",
+    repo : "https://github.com/rafaVls/Weatherly"
+  },
+  {
+    img : "/images/projects/homepage.png",
+    title : "Personal Website",
+    description :
+        "This is a website made using Next.js, a static site generator; and Ghost, a headless cms for the blog section.",
+    stack : [ "Node", "Nextjs", "Ghost CMS", "TypeScript" ],
+    link : "/",
+    repo : "https://github.com/rafaVls/personal-website"
+  },
+  {
+    img : "/images/projects/calculator.png",
+    title : "Calculator app",
+    description :
+        "This app is a solution to the Frontend Mentor calculator challenge. Some of the math functionailty is still a work in progress.",
+    stack : [ "HTML", "CSS", "Typescript" ],
+    link : "https://calculator-site.vercel.app/",
+    repo : "https://github.com/rafaVls/calculator_site"
+  }
 ];
 
 export default Projects;
